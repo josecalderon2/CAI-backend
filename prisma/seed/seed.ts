ffff1ca4f52d66af77abf19aa1ba37fe76ca4ee3
car020202 <carlitosyanez9@gmail.com>
2025-09-22 10:37:50 -0600

    modulos creadoslistos para desarrollo, seed creado con cargos administrativos

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -2,12 +2,15 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+//ejemplo de como insertar datos iniciales en la base de datos
+// como correr el comando: npx run prisma:seed
 async function main() {
-  // Usuarios de ejemplo
-  await prisma.user.createMany({
-    data: [],
-    skipDuplicates: true,
+  await prisma.cargo_administrativo.createMany({
+    data: [{ nombre: 'Admin' }, { nombre: 'P.A' }, { nombre: 'Orientador' }],
+    skipDuplicates: true, // esto evita duplicados
   });
+
+  console.log('Cargos insertados correctamente');
 }
 
 main()
