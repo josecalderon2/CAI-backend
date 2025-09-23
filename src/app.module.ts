d58e6e42dae55a76403446e53cf9d3a0f55e9621
RodolfoRivasRodriguez <rodolfo.rivas@catolica.edu.sv>
2025-09-23 12:58:46 -0700

    Correccion para correr

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdministrativoModule } from './administrativo/administrativo.module';
@@ -6,7 +7,12 @@ import { AuthModule } from './auth/auth.module';
 import { OrientadorModule } from './orientador/orientador.module';
 
 @Module({
-  imports: [AdministrativoModule, AuthModule, OrientadorModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AdministrativoModule,
+    AuthModule,
+    OrientadorModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
