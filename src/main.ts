aac087351be2d20d566ecd6596da7db0623c2b25
car020202 <carlitosyanez9@gmail.com>
2025-09-22 21:54:30 -0600

    CORS

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,27 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Configuración Swagger
+
+  // HABILITAR CORS para Vite (5173)
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false, // true solo si usaras cookies; con Bearer puede ser false
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle('API Colegio Amigos de Israel')
     .setDescription('Documentación de la API para pruebas')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
