c5752b5c1a01cbb5b3fa2046aaa804b42fdc4828
Kruma <115597425+josecalderon2@users.noreply.github.com>
2025-09-29 11:33:19 -0600

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,26 @@ import { AdministrativoModule } from './administrativo/administrativo.module';
 import { AuthModule } from './auth/auth.module';
 import { OrientadorModule } from './orientador/orientador.module';
 import { MailModule } from './mail/mail.module';
+import { AlumnosModule } from './alumnos/alumnos.module';
+import { ResponsablesModule } from './responsables/responsables.module';
+import { ParentescoModule } from './parentesco/parentesco.module';
 
 @Module({
   imports: [
+<<<<<<< HEAD
     ConfigModule.forRoot({ isGlobal: true }),
     AdministrativoModule,
     AuthModule,
     OrientadorModule,
+=======
+    AdministrativoModule,
+    AuthModule,
+    OrientadorModule,
+    MailModule,
+    AlumnosModule,
+    ResponsablesModule,
+    ParentescoModule,
+>>>>>>> Carlos
   ],
   controllers: [AppController],
   providers: [AppService],
