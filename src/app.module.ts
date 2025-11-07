0dc13d6036b05515888829828dd3b46db53ae3c2
car020202 <carlitosyanez9@gmail.com>
2025-11-07 07:58:08 -0600

    eliminacion de evaluacioes

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,7 +27,6 @@ import { ResumenModule } from './asistencias/resumen/resumen.module';
 import { ConductaModule as ConductaAsistenciaModule } from './asistencias/conductaAsistencia/conductaAsistencia.module';
 import { AsistenciaModule } from './asistencias/asistencia.module';
 import { TipoEvaluacionModule } from './tipo_evaluacion/tipo-evaluacion.module';
-import { EvaluacionesModule } from './evaluaciones/evaluaciones.module';
 
 import { PromocionesModule } from './promociones/promociones.module';
 @Module({
@@ -56,7 +55,6 @@ import { PromocionesModule } from './promociones/promociones.module';
     ConductaAsistenciaModule,
     AsistenciaModule,
     TipoEvaluacionModule,
-    EvaluacionesModule,
     PromocionesModule,
     ScheduleModule.forRoot(),
   ],
--- src/evaluaciones/dto/update-evaluacion.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateEvaluacionDto } from './create-evaluacion.dto';
-
-export class UpdateEvaluacionDto extends PartialType(CreateEvaluacionDto) {}
