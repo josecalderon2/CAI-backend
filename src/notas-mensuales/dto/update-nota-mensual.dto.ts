60e9de17333185804d2b2324c6d0fe961fdf9c19
Miguel Leiva <miguel.leiva@catolica.edu.sv>
2025-11-11 12:10:33 -0600

    Revert "Notas V0.1"
    This reverts commit 3492530656ffddc02361508c08048358bae9865d.

--- src/notas-mensuales/dto/update-nota-mensual.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateNotaMensualDto } from './create-nota-mensual.dto';
-import { OmitType } from '@nestjs/swagger';
-
-// Omitimos los campos que no deberían cambiar en una actualización
-export class UpdateNotaMensualDto extends PartialType(
-  OmitType(CreateNotaMensualDto, ['id_alumno', 'id_asignatura', 'mes', 'anio'] as const),
-) {}
